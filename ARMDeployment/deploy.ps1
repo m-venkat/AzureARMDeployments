ab553501b0a2b15f327140870ae981f1d3d2457c
Venkatesh Muniyandi <venkatesh_muniyandi@dialog.com.au>
2019-02-22 19:39:44 +1100

    Comitting

--- ARMDeployment/deploy.ps1
+++ ARMDeployment/deploy.ps1
@@ -91,20 +91,6 @@ if($resourceGroup)
 Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
     New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
 
-
-#if(!$resourceGroup)
-#{
-#    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
-#    if(!$resourceGroupLocation) {
-#        $resourceGroupLocation = Read-Host "resourceGroupLocation";
-#    }
-#    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
-#    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
-#}
-#else{
-#    Write-Host "Using existing resource group '$resourceGroupName'";
-#}
-
 # Start the deployment
 Write-Host "Starting deployment...";
 if(Test-Path $parametersFilePath) {
